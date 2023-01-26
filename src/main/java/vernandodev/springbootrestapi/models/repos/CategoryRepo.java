7de0043c6a25270cc7e9ff1b815a7fabba3cf0e9
Richo Vernando <66782281+vernandodev@users.noreply.github.com>
2023-01-26 11:48:51 +0700

    custom query findProductByName with JPAQL

--- src/main/java/vernandodev/springbootrestapi/models/repos/CategoryRepo.java
+++ src/main/java/vernandodev/springbootrestapi/models/repos/CategoryRepo.java
@@ -1,7 +1,12 @@
 package vernandodev.springbootrestapi.models.repos;
 
+import jakarta.websocket.server.PathParam;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import vernandodev.springbootrestapi.models.entities.Category;
+import vernandodev.springbootrestapi.models.entities.Product;
 
 public interface CategoryRepo extends CrudRepository<Category, Long> {
+    @Query("SELECT p FROM Product p WHERE p.name = :name") // anotasi query untuk custom query, JPAQL
+    public Product findProductByName(@PathParam("name") String name);
 }
